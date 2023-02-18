10ec1c0141d1836a87925859881e63654e745552
Parth <parth.sce21@sot.pdpu.ac.in>
2023-02-18 17:22:40 +0530

    add:provided navbar

---
+++ Main/frontend/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+  extend: {
+    colors: {
+      primary: "#C9872B",
+      secondary: {
+        DEFAULT: "#B69575",
+        opacity: "#161616",
+        15: "rgba(182, 149, 117, 0.15)",
+      },
+      tertiary: "#0F0F0F",
+      inputFieldColor: "rgba(0, 0, 0, 0.06)",
+    },
+    gridTemplateColumns: {
+      "contactSection": "1fr 2fr",
+    },
+    fontFamily: {
+      Lato: ["Lato", "sans-serif"],
+      Bebas: ["Bebas Neue", "sans-serif"],
+      Yeseva: ["Yeseva One", "sans-serif"],
+      Abel: ["Abel", "sans-serif"],
+      Gloock:["Gloock","sans-serif"]
+    },
+  },
+  plugins: [],
+}
+}
+
+ 
