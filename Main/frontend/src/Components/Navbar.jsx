03adeebe683f3f67d643ec14b90cc37e902f0659
Parth <parth.sce21@sot.pdpu.ac.in>
2023-03-14 01:57:24 +0530

    Add:drop down + intro pages

--- Main/frontend/src/Components/Navbar.jsx
+++ Main/frontend/src/Components/Navbar.jsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   const [open,notOpen] = useState(false);
+  const [algOpen,algNotOpen] = useState(false);
   
+  const handleAlgo = () =>{
+    algNotOpen(!algOpen);
+  }
   const handleClickHamb = () =>{
     notOpen(!open);
     
@@ -19,12 +23,24 @@ export const Navbar = () => {
         </div>
         <div className="hidden md:flex justify-between p-7 ml-auto text-[20px] lg:gap-10 lg:text-[22px]  z-40 ">                  
             <div className='p-3 cursor-pointer hover:text-red-600'><Link to="/">Home</Link></div>
-            <div className='p-3 cursor-pointer hover:text-red-600'>Algorithms</div>
+            <div className='p-3 cursor-pointer hover:text-red-600' onClick={handleAlgo}>Algorithms</div>
             <div className='p-3 cursor-pointer hover:text-red-600'><Link to="/about">About us</Link></div>
         </div>
         <div className='navbar bg-[#131316] md:hidden p-2 cursor-pointer' onClick={handleClickHamb}>
         ▼
         </div>
+         <div className={`  text-[25px] absolute w-1/5 bg-[#131316] pt-4 transition-transform duration-500 ease-in  ${algOpen ? "translate-x-[375%]  translate-y-[85%]  ": " translate-y-[-85%]  translate-x-[375%]"} `}>
+            <div className='hover:text-red-600'><Link to="roundrobin"> Round Robin</Link></div>
+            <div className='hover:text-red-600'><Link to="/bankersAlgorithm"> Banker's</Link></div>
+            <div className='hover:text-red-600'> <Link to="/scan-cscan">SCAN-CSCAN</Link></div>
+            <div className='hover:text-red-600'><Link to="/MRU">MRU</Link> </div>
+            {/* bg-[#131316] */}
+
+
+         </div>
+
+
+
         <div className={` text-[25px] absolute w-full bg-[#131316]  pt-3 transition-transform duration-500 ease-in  ${open ? "translate-y-0": "translate-y-[-200%]"} `}>                  
             <div className='p-3 cursor-pointer duration-300 hover:text-red-600'><Link to="/">Home</Link></div>
             <div className='p-3 cursor-pointer duration-300 hover:text-red-600'>Algorithms</div>
