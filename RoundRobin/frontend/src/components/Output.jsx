a9969307a1056e5ae13decd3d7ed20036571192e
Priyank Vaidya <96229416+Priyank-Vaidya@users.noreply.github.com>
2023-04-07 11:40:16 +0530

    Update Output.jsx

--- RoundRobin/frontend/src/components/Output.jsx
+++ RoundRobin/frontend/src/components/Output.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Container, Box, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody, Typography, Divider } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import GanttChart from "react-gantt-chart";
 
 function Output() {
     const [processes, setProcesses] = React.useState([]);
